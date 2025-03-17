8ad7ce3a8bac9638423dbfb6114e69a33a93d03e
Erick <ericklmduarte@gmail.com>
2025-03-17 15:38:21 -0300

    fix beastudent

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 
 function showmenu() {
     var x = document.getElementById("abas_mobile");
+    var elStyle = window.getComputedStyle(element);
+    var topValue = elStyle.getPropertyValue("top").replace("px", "");
     if (x.style.display === "block") {
         x.style.display = "none";
     }  
     else
     {
         x.style.display = "block";
+        element.style.top = (250 - 20) + "px";
     }
 
 }
