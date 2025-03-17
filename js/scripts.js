c8ea119361005f2080d85b8b0127451dd5464fa0
Erick <ericklmduarte@gmail.com>
2025-03-17 10:44:53 -0300

    responsive menu

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function showmenu() {
+    var x = document.getElementById("abas");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
