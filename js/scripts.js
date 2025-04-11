f30db2bd29c383f2cb6d7afe79e906bcecfc14ce
Erick <ericklmduarte@gmail.com>
2025-04-11 14:21:44 -0300

    Moving Absolute Position Texts

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 
 function showmenu() {
     var x = document.getElementById("abas_mobile");
-    var elStyle = window.getComputedStyle(element);
-    var topValue = elStyle.getPropertyValue("top").replace("px", "");
+    const movetexto = document.getElementsByClassName("chamada_textos");
+
+
     if (x.style.display === "block") {
         x.style.display = "none";
     }  
     else
     {
         x.style.display = "block";
-        element.style.top = (250 - 20) + "px";
+    }
+    if (movetexto.length > 0)
+    {
+         if(x.style.display ==="block")
+        {
+             movetexto[0].style.top = '340px';
+        }
+        else
+        {
+            movetexto[0].style.top = '' ;
+        }
     }
 
 }
